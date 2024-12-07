844bd41b237a86848f6492b9bc9ac18fa994fd5e
tomzyhub <tomzy337@gmail.com>
2024-12-07 14:51:37 +0100

    navbar both mobile and desktop view completed

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// function for toggle
+
+function toggleMenu(){
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+
+    menu.classList.toggle('open'
+        icon.classList.toggle('open')
+    )
+
+}
