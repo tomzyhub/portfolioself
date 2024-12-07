f1af02bbb736f748687266850d64bf42052f96ff
tomzyhub <tomzy337@gmail.com>
2024-12-07 15:47:27 +0100

    mobile navigation was going up too much the csss was fixed so i update the code.

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
 // function for toggle
 
-function toggleMenu(){
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
 
-    menu.classList.toggle('open'
-        icon.classList.toggle('open')
-    )
-
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
